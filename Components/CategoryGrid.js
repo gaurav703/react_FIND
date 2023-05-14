00b514c3f3443adffe03c53d57687ac6936cde7d
gaurav703 <111629507+gaurav703@users.noreply.github.com>
2023-05-14 13:34:40 +0530

    first change

--- Components/CategoryGrid.js
+++ Components/CategoryGrid.js
@@ -1,64 +1,64 @@
-import { Pressable, View ,Text,StyleSheet,Platform} from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 
-function CategoryGrid({title, color, onPress}){
+function CategoryGrid({ title, color, onPress }) {
 
-return (
-  
-<View style = {styles.griditem} >
-    <Pressable style ={({ pressed })=> [styles.button, pressed ? styles.buttonPress : null]} android_ripple={ {color :'#ccc'} } onPress = {onPress} 
-    //onPress = {onPress} is used to navigate to the next screen.
-    // android_ripple is used to give ripple effect in android.
-    // android_ripple is not working in ios.
-    // android_ripple is not working in android for shadow effect.
-    >
-        <View style={[styles.gridpressableView,{ backgroundColor: color}]}>
+  return (
 
-            <Text style = {styles.griditemtext}>{title}</Text>
-            
+    <View style={styles.griditem} >
+      <Pressable style={({ pressed }) => [styles.button, pressed ? styles.buttonPress : null]} android_ripple={{ color: '#ccc' }} onPress={onPress}
+      //onPress = {onPress} is used to navigate to the next screen.
+      // android_ripple is used to give ripple effect in android.
+      // android_ripple is not working in ios.
+      // android_ripple is not working in android for shadow effect.
+      >
+        <View style={[styles.gridpressableView, { backgroundColor: color }]}>
+
+          <Text style={styles.griditemtext}>{title}</Text>
+
         </View>
-    </Pressable>
-</View>
-);
+      </Pressable>
+    </View>
+  );
 }
 
-export default CategoryGrid; 
+export default CategoryGrid;
 
 const styles = StyleSheet.create({
-  griditem:{
+  griditem: {
     flex: 1,
     margin: 15,
     height: 150,
-    elevation: 2,
+   // elevation: 2,
     borderRadius: 10,
-   // backgroundColor helps in ios for shadow effect and android for elevation effect . backgroundColor is not working in android for shadow effect.  
-  
+    // backgroundColor helps in ios for shadow effect and android for elevation effect . backgroundColor is not working in android for shadow effect.  
+
     shadowColor: 'black',
     shadowOpacity: 0.26,
     overflow: Platform.OS === 'andriod' ? 'hidden' : 'visible', // overflow is used to hide the shadow effect which is coming out of the box.
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
   },
 
   button: {
-    flex:1,
-},
-buttonPress: {
-opacity:0.26,
-},
+    flex: 1,
+  },
+  buttonPress: {
+    opacity: 0.26,
+  },
 
-  gridpressableView:{
-   alignItems: 'center',
+  gridpressableView: {
+    alignItems: 'center',
     justifyContent: 'center',
-    flex:1,
+    flex: 1,
     borderRadius: 10,
   },
-    griditemtext:{
+  griditemtext: {
     fontSize: 20,
     fontWeight: 'bold',
     color: 'black',
     textAlign: 'center',
-},
+  },
 
 
 
--- Screen/Meal.js
+++ Screen/Meal.js
@@ -1,37 +1,43 @@
 import { MEALS } from "../data/dummy";
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 // route is property to use data moving from one screen to another .
 
-function Meal({route}){
+function Meal({ route }) {
 
-   const catId = route.params.categoryId;
+  const catId = route.params.categoryId;
 
-//    const displayedMeals = MEALS.filter( ( mealItem ) => { 
-//     return  mealItem.categoryIds.indexOf(catId) >= 0; 
-//    });
 
-//     function renderMealItem(itemData){
-//       return  <MealItem title = {itemData.item.title}/> ;  
-//     }
-    return(
-        <View style = {styles.container}>
-          <Text>
-            {catId}
-          </Text>
+  //    const displayedMeals = MEALS.filter( ( mealItem ) => { 
+  //     return  mealItem.categoryIds.indexOf(catId) >= 0; 
+  //    });
 
-        </View>
-    );
-   
+
+  //     function renderMealItem(itemData){
+  //       return  <MealItem title = {itemData.item.title}/> ;  
+  //     }
+
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        {catId}
+      </Text>
+
+    </View>
+  );
+
 }
 
 export default Meal;
 
 const styles = StyleSheet.create({
-container :{
+  container: {
     flex: 1,
     padding: 16,
-},
+    backgroundColor: '#fff',
+
+  },
 
 
 });
